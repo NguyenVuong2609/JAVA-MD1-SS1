e8b6210c75cb93f1fd65a64df90240fa48127d8c
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-02-28 11:12:35 +0700

    2802

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Nhập vào 1 số: ");
+//        int newNumber = scanner.nextInt();
+//        System.out.println(newNumber + 30);
+
+        // Tìm số lớn nhất trong 3 số:
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Nhập vào số A:");
+//        int a = scanner.nextInt();
+//        System.out.println("Nhập vào số B:");
+//        int b = scanner.nextInt();
+//        System.out.println("Nhập vào số C:");
+//        int c = scanner.nextInt();
+//
+//        int max = 0;
+//        max = a > b ? (a > c ? a : c) : (b > c ? b : c);
+//        System.out.println("Số lớn nhất là: " + max);
+
+        // * Hiển thị ngày trong máy tính
+        Date date = new Date();
+        System.out.println("Time now:" + date);
+
+
+    }
+}
